9f8663692ffddeaa33b44d8d3b8cf26f568435ab
gysiedebruyn <jan@4debruyns.com>
2015-11-04 22:17:07 +1000

    Django Source File

--- trydjango18/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\u2\xdemo\pythonbetarocket\pyprojects\trydjango18\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
